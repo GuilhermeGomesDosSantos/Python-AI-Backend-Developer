19ce3d742170f3cfcaae6793fef25027c49bd58f
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-04-06 13:20:07 -0300

    Conversão de tipos

---
+++ src/Conhecendo a Linguagem de Programação Python/convertendo_tipos.py
@@ -0,0 +1,13 @@
+print (int (1.435)) # Converte um valor float em inteiro
+print (int ("10")) # Converte uma string em inteiro
+print (float ("10.10")) # Convertendo uma string com valor float para o tipo float
+print (float (100)) # Converte um valor inteiro em flutuante
+print (str (10.10)) # Converte um valor flutuante em inteiro
+
+valor = 10
+valor_str = str (valor) # Converte a variavel em string
+print (type (valor)) # retorna o tipo da variavel
+print (type (valor_str))
+
+print (100 / 2) # retorna o valor em tipo flutuante
+print (100 // 2) # retorna o valor em tipo inteiro
