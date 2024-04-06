854edd3fed4c65ce4372b24ea6e1e64efd23a7e9
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-04-06 12:11:12 -0300

    Variáveis e constantes

---
+++ src/Conhecendo a Linguagem de Programação Python/variaveis_constantes.py
@@ -0,0 +1,20 @@
+nome = 'Guilherme'
+idade = 21
+
+print(nome, idade)
+
+nome, idade = 'GDS', 30
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+print(limite_saque_diario)
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES);
+
+BRAZILIAN_STATES = 10
+
+print(BRAZILIAN_STATES);
