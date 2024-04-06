149e0168d61d7d977668c38536b825e1b2fcd0b6
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-04-06 13:32:03 -0300

    Funções de entrada e saída

---
+++ src/Conhecendo a Linguagem de Programação Python/print_input.py
@@ -0,0 +1,8 @@
+nome = input ('Informe o seu nome nome: ')
+idade = input ('Informe a sua idade: ')
+
+print(nome, idade)
+# print("teste", end=" ")
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
