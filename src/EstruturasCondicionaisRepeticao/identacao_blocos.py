c23bc6dfc731abeaae5e50325dd5ef7a5f1317ff
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-04-27 18:24:57 -0300

    Indentação e blocos

---
+++ src/EstruturasCondicionaisRepeticao/identacao_blocos.py
@@ -0,0 +1,18 @@
+def sacar (valor):
+    saldo = 500
+
+    if saldo >= valor:
+
+        print("valor sacado!")
+        print("retire o seu dinheiro na boca do caixa")
+
+    print("Obrigado por ser nosso cliente, tenho um bom dia!")
+
+# sacar(300)
+sacar(1000)
+
+# Codigo mal identado
+# def depositar (valor):
+#     saldo = 500
+
+# saldo += ValueError
