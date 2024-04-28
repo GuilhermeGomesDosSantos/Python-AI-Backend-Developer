4b69a7edb159581bf0d884b54d6b763f67886101
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-04-28 10:39:52 -0300

    Fatiamento de string

---
+++ src/Manipulando Strings/string_3.py
@@ -0,0 +1,15 @@
+nome = "Guilherme Gomes dos Santos"
+
+print(nome[0]) # retorna a primeira posição do indice da variavel nome
+
+print(nome[:19])# retorna des do indice 0 da variavel até o indice 19
+
+print(nome[10:])# retorna des do indice 10 da variavel até o final do indice
+
+print(nome[10:16])# retorna a partir do indice 10 da variavel até o indice 16
+
+print(nome[10:16:2])# retorna a partir do indice 10 da variavel até o indice 16 porem vai pulando de 2 em 2
+
+print(nome[:]) # irá imprimir a variavel completa
+
+print(nome[:: -1])# retorna o inverso da variavel
